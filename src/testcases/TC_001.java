9ec63806847a559546e44a29204138ec9a436400
Deepak Chandra <deepak.chandra@elixirct.com>
2023-01-20 10:30:14 +0530

    Intial Commit

---
+++ src/testcases/TC_001.java
@@ -0,0 +1,22 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void testcase1() {
+		
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://uat.enalytix.com/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("sup090123@yopmail.com");
+		driver.findElement(By.id("password")).sendKeys("India@123");
+		driver.quit();
+		
+	}
+
+}
